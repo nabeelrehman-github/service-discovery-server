e2794b733e3792888126741fa824f3b77d7f1baa
Nabeel Rehman <Nabeel Rehman@192.168.10.10>
2021-07-01 13:42:10 +0500

    added get mapping

--- src/main/java/com/servicediscovery/server/UsersController.java
+++ src/main/java/com/servicediscovery/server/UsersController.java
@@ -1,11 +1,14 @@
 package com.servicediscovery.server;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UsersController {
+
+	@GetMapping
 	public String getUsers() {
 		return "User 1, User 2, User 3, User 4";
 	}
